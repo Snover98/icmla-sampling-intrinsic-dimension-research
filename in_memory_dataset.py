f1bbaf75ba646d6bd9db299b60c9c6b0004bfbe2
Snover98 <Assaf98@Gmail.com>
2025-03-16 18:28:35 +0200

    Dobby is a free elf!

--- in_memory_dataset.py
+++ in_memory_dataset.py
@@ -1,16 +1,22 @@
 from typing import Any
 
+import torch
+
 from torch.utils.data import Dataset
+
 
 class InMemoryDataset(Dataset):
     def __init__(self, other_ds: Dataset):
-        self.data = list()
-        self.labels = list()
+        data = list()
+        labels = list()
 
         for idx in range(len(other_ds)):
             d, l = other_ds.__getitem__(idx)
-            self.data.append(d)
-            self.labels.append(l)
+            data.append(d)
+            labels.append(l)
+
+        self.data = torch.stack(data)
+        self.labels = labels
 
     def __getitem__(self, index: int) -> tuple[Any, Any]:
         """
